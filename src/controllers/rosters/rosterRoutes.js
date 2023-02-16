94cc368ddddfbd6339a02bbefc31ebb16f525ef8
Laura <12727@coderacademy.edu.au>
2023-02-16 11:53:01 +1000

    roster virtual

--- src/controllers/rosters/rosterRoutes.js
+++ src/controllers/rosters/rosterRoutes.js
@@ -1,16 +1,18 @@
 const express = require("express")
 const rosterRouter = express.Router()
-const {getRosters, getRosterById} = require("./rosterControllers")
+const {getRosters, getRosterById, 
+    //getRosterByUserIdWithShiftInfo
+} = require("./rosterControllers")
 
 
 
-rosterRouter.get("/", (request, response) => {
-    const rosters = getRosters()
+rosterRouter.get("/", async (request, response) => {
+    const rosters = await getRosters()
     response.json(rosters)
 })
 
-rosterRouter.get("/:rosterId", (request, response) => {
-    const roster = getRosterById(request.params.rosterId)
+rosterRouter.get("/:rosterId", async (request, response) => {
+    const roster = await getRosterById(request.params.rosterId)
     if(!roster) {
         response.json({
         data: "roster doesn't exist"
@@ -20,3 +22,19 @@ rosterRouter.get("/:rosterId", (request, response) => {
 })
 
 module.exports = rosterRouter
+/*
+rosterRouter.get("/:rosterId", async (request, response) => {
+    if (request.query.getShiftInfo) {
+        roster = await getRosterByUserIdWiithShiftInfo(request.params.UserId)
+    } else {
+        const roster = await getRosterById(request.params.rosterId) 
+    }
+
+    if(!roster) {
+        response.json({
+        data: "roster doesn't exist"
+        }, 404)
+    }
+    response.json(roster)
+})
+*/
--- src/models/roster.js
+++ src/models/roster.js
@@ -1,12 +1,26 @@
 const mongoose = require("mongoose")
 
 const RosterShiftSchema = new mongoose.Schema({
-    shift_id: String,
+    shift_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Shift"
+    },
+}, {
+    toJSON: {
+        virtuals: true,
+    }
 })
 
 const RosterSchema = new mongoose.Schema({
     user_id: String, 
     shifts: [RosterShiftSchema],
+})
+
+RosterShiftSchema.virtual("shift", {
+    localField: "shift_id",
+    foreignField: "_id",
+    ref:"Shift",
+    justOne: true,
 })
 
 const Roster = mongoose.model("Roster", RosterSchema)
