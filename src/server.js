1541376c2dbcfa6010c6ff06e680f3d7076e8d4f
Laura <12727@coderacademy.edu.au>
2023-03-05 11:42:09 +1000

    error response.json

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ app.use("/rosters", rosterRouter)
 app.use("/users", userRouter)
 
 app.use(function(error, request, response, next) {
-    console.error(error)
+    response.json(error)
     response.status(500).send({message: "Something went wrong"})
 })
 
